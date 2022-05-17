045839ecb8dd191efa0eb84e8fcec43c936aa7fb
Mohammad Rahman <mbrahman@gmail.com>
2022-05-17 16:58:42 -0400

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+require('dotenv').config()
+
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
@@ -23,6 +25,8 @@ mongoose.connect(MONGODB_URI, {
 
 // routes
 app.use(require("./routes/api.js"));
+
+mongoose.set('debug', true)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
